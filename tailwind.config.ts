9696ae10a85b7d85a480bd9ebbefab17e6a73bb6
G4zzor <108358553+G4zzor@users.noreply.github.com>
2026-04-07 21:54:40 +0200

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1A73E8',
+        secondary: '#4285F4',
+        dark: '#202124',
+        light: '#F8F9FA',
+        accent: '#34A853',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
